a03d18043143c67ea8e93b1276d167d870861712
jcvdav <juanarlos.villader@gmail.com>
2016-03-08 18:44:46 -0800

    Added @zebos1 Extantify function to the repository for our functions (no changes made).

---
+++ R/Extantify.R
@@ -0,0 +1,42 @@
+#' extantify
+#'
+#' subsetcolumns and extantifies the IUCN long data table based on desired output
+#'
+#' @param subsetcolumns (a vector of columns to subset based off of)
+#'
+#' @param factors (Indicate what factors to select for the corresponding subset columns. Note this should be a vector or list of same length as subsetcolumns)
+#'
+#' @return extant (Extant fish species for a given subset)
+#'
+#' @author Niko Hartline, Fish-ecol.github.io
+#'
+#'
+#'
+Extantify=function(subsetcolumns=NA,factors=NA){
+  
+  if(length(subsetcolumns)!=length(factors)){stop("Error: subsetcolumns input is not the same length as the factors input. These should match in ")}
+  
+  #Load in original data set- note this will take a couple seconds
+  LongData=read.csv("./Data/Long Format Collected Fish Information IUCN")
+  
+  #Load in land locked data set (ISO3 codes)
+  Landlocked=read.csv("./Data/Landlocked.csv")
+  
+  #Exclude landlocked countries
+  OceanNationsOnly=LongData[!LongData$CountryISO3%in%Landlocked$Landlocked_Countries,]
+  
+  #Find the extant classifications: 1- Introduced 2- Native 6- Reintroduced
+  extantIDX=grepl("1|2|6",as.character(OceanNationsOnly$Species_Presence))
+  
+  extant=OceanNationsOnly[extantIDX,-c(1,5,6)]
+  
+  if(is.na(subsetcolumns[1])){return(extant)}else{
+    for(i in 1:length(subsetcolumns)){
+      IDXl=grepl(paste(factors[[i]],collapse="|"),extant[[subsetcolumns[[i]]]])
+      extant=extant[IDXl,]
+    }
+    return(extant)
+  }
+}
+
+
